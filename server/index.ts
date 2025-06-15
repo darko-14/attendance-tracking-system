dab0287f252dc0ab1eebe9af6336a78d718ee190
Darko <darko@tgn.mk>
2025-06-15 23:32:43 +0200

    run client in docker

--- server/index.ts
+++ server/index.ts
@@ -3,8 +3,10 @@ import Fastify from 'fastify';
 const app = Fastify({ logger: true });
 
 const start = async () => {
+    const port: string = process.env.PORT || '3001';
     try {
-        await app.listen({ port: 3001 });
+        await app.listen({ port: parseInt(port) });
+        app.log.info(`Server listening on port: ${port}`);
     } catch (error) {
         app.log.error(error);
         process.exit(1);
